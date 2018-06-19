b98d7cc806cd2e57a7eff83c01447d2967f227f3
Taspyn <taspyn.akirachix@gmail.com>
2018-06-19 17:16:26 +0300

    the fat arrow functions code

---
+++ fatarrowadd.js
@@ -0,0 +1,17 @@
+// var sum = function (num1,num2){
+// 	return num1+num2;
+// };
+// console.log(sum (7,12))
+
+
+// var sum=(num1,num2)=>{
+
+// return num1+num2;
+// }
+// console.log(sum(7,12))
+
+
+
+var sum=(num1,num2)=>num1+num2;
+
+console.log(sum(7,12));
---
+++ fatarrowsubtract.js
@@ -0,0 +1,15 @@
+// var subract= function(num1,num2){
+// 	return num2-num1;
+// }
+// console.log(subract(30,12))
+
+
+// var subract=(num1,num2)=>{
+// 	return num2-num1;
+// }
+// console.log(subract(30,12))
+
+
+var subract=(num1,num2)=>num2-num1;
+
+console.log(subract(30,12))
